324393871cbb324d5e3d3b4d19cfaa53dfe6c810
hayashitoshiki <tosikihayasi@outlook.jp>
2020-09-09 16:24:00 +0900

    ログイン画面修正

--- app/src/main/java/com/example/musicdictionaryandroid/ui/login/SignInFragment.kt
+++ app/src/main/java/com/example/musicdictionaryandroid/ui/login/SignInFragment.kt
@@ -1,17 +1,20 @@
 package com.example.musicdictionaryandroid.ui.login
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.musicdictionaryandroid.R
 import com.example.musicdictionaryandroid.databinding.FragmentSignInBinding
 import com.example.musicdictionaryandroid.model.util.Status
+import kotlinx.android.synthetic.main.fragment_sign_in.view.*
 
 
 class SignInFragment : Fragment() {
@@ -23,6 +26,24 @@ class SignInFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentSignInBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_in, container, false)
         binding.viewModel = viewModel
+
+        // editTextフォーカス制御
+        binding.root.email_edit_text.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        }
+        binding.root.password_edit_text.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        }
+        binding.root.setOnTouchListener { v, event ->
+            binding.root.requestFocus()
+            v?.onTouchEvent(event) ?: true
+        }
         return binding.root
     }
 
--- app/src/main/java/com/example/musicdictionaryandroid/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/example/musicdictionaryandroid/ui/login/SignUpFragment.kt
@@ -1,17 +1,21 @@
 package com.example.musicdictionaryandroid.ui.login
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.musicdictionaryandroid.R
 import com.example.musicdictionaryandroid.databinding.FragmentSignUpBinding
 import com.example.musicdictionaryandroid.model.util.Status
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.fragment_sign_up.view.*
 
 class SignUpFragment : Fragment() {
 
@@ -23,6 +27,30 @@ class SignUpFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentSignUpBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up, container, false)
         binding.viewModel = viewModel
+
+        // editTextフォーカス制御
+        binding.root.email_edit_text.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        }
+        binding.root.password_edit_text.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        }
+        binding.root.name_edit_text.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        }
+        binding.root.setOnTouchListener { v, event ->
+            binding.root.requestFocus()
+            v?.onTouchEvent(event) ?: true
+        }
         return binding.root
     }
 
